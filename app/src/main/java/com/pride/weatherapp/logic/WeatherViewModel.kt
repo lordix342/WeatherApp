e7744ec91e18765da766e754d9c556da66d81b13
lordix342 <105501284+lordix342@users.noreply.github.com>
2022-06-23 15:13:24 +0300

    CurrentBlock finish

--- app/src/main/java/com/pride/weatherapp/logic/WeatherViewModel.kt
+++ app/src/main/java/com/pride/weatherapp/logic/WeatherViewModel.kt
@@ -1,6 +1,5 @@
 package com.pride.weatherapp.logic
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
